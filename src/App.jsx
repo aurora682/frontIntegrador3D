acdf7ac8cadba86a3e0ad3ff68aa1da0b13858e6
aurora682 <140974950+aurora682@users.noreply.github.com>
2024-07-08 13:24:23 -0600

    vista cliente terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css';
+import Login from './organismos/login.jsx';
+import Productos from './organismos/productos.jsx';
+import Pedidos from './organismos/pedidos.jsx';
+import Historial from './organismos/historial.jsx';
 import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      //element: ,
+      element:<Login/> ,
     },
+    {
+      path:"/productos",
+      element:<Productos/>,
+    },
+    {
+      path:"/pedidos",
+      element:<Pedidos/>,
+    },
+    {
+      path:"/historial",
+      element:<Historial/>,
+    }
+
   ])
   
   return (
